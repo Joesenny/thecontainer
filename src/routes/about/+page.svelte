9a6c4ff685232b832f6a19cdfea6d9371ca5f993
joe senenfelder <est2020.joe@gmail.com>
2023-07-09 02:06:04 -0500

    about page updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,11 +3,71 @@
 </h1>
 <p> <small> a joe senny blog </small> </p>
 
-<h3> Welcome to theContainer website! This page is designed 
-    to serve as an RSS feed. An RSS feed (if you do not know) 
-    lets you gather articles and information cleanly and organized. 
+<h3> Welcome to theContainer website!</h3>
+ <br> 
+<p> <span>This page is designed 
+    to serve articles to an RSS feed.</span>An RSS (Really Simple Syndication) 
+    feed is a standardized format for delivering regularly updated 
+    content from a website or online platform. <br> It allows users 
+    or subscribers to stay informed about new articles, blog posts,
+     podcasts, news updates, and other types of content published by
+    their favorite websites or sources.. <br> <br>
     However, I have exciting plans to expand and enhance the site
      over time as I delve deeper into the world of Svelte.
     <br> <br>
+</p>
 
+<h3>
+    How to subscribe to theContainer RSS feed
 </h3>
+<small> Instrucrions Provided by non other than chatGIPPTY!!!</small>
+<ol>
+    <li>Choose a feed reader or aggregator.</li>
+    <p> <a href='https://feedly.com/' target='_blank' >Feedly </a> | <a href='https://newsblur.com/' target='_blank'>
+     NewsBlur</a> |<a href='https://theoldreader.com/' target='_blank'>The Old Reader </a></p>
+    <li>Locate the subscription option.</li>
+    <li>Enter the RSS feed URL (e.g., "https://thecontainer.netlify.app/rss.xml").</li>
+    <li>Confirm the subscription.</li>
+    <li>Customize preferences if available.</li>
+    <li>Access updated content in your feed reader.</li>
+  </ol>
+
+<style>
+    
+ @keyframes colorTransition {
+            0% { color: red; }
+            25% { color: blue; }
+            50% { color: green; }
+            75% { color: orange; }
+            100% { color: red; }
+          }
+
+    span {
+        color: #0070f3;
+        
+    }
+    small {
+        color: #08c1d2;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: underline;
+    }
+
+    a:hover {
+        animation: colorTransition 2s infinite;
+    }
+
+
+  
+    ol {
+    list-style-type: decimal;
+    padding-left: 1.5em;
+    margin-top: 0;
+  }
+
+  li {
+    margin-bottom: 0.5em;
+  }
+</style>
